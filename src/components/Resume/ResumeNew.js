5ab7357d6d8005dca79a48dab6e8c8b91f828677
TravisHuy <honhathuy098@gmail.com>
2024-08-10 00:34:22 +0700

    fix pdf again

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import honhathuy from '../Assets/honhathuy.pdf';
+import pdf from "../../Assets/../Assets/honhathuy.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
